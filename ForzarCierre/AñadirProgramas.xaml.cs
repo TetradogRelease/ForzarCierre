477f966396c425f11195d009637744c505da1bf4
gabriel <tetradog@gmail.com>
2018-08-20 12:59:10 +0200

    subir programa

---
+++ ForzarCierre/AñadirProgramas.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ForzarCierre
+{
+    /// <summary>
+    /// Lógica de interacción para AñadirProgramas.xaml
+    /// </summary>
+    public partial class AñadirProgramas : Window
+    {
+        public AñadirProgramas(IList<string> programasAñadidos)
+        {
+            InitializeComponent();
+            for (int i = 1; i < programasAñadidos.Count; i++)
+                lstProgramasAñadidos.Items.Add(programasAñadidos[i]);
+            btnActualizarLista_Click();
+        }
+
+        private void btnActualizarLista_Click(object sender = null, RoutedEventArgs e = null)
+        {
+            System.Diagnostics.Process[] procesosAbiertos = System.Diagnostics.Process.GetProcesses();
+            lstProgramasAbiertos.Items.Clear();
+            for (int i = 0; i < procesosAbiertos.Length; i++)
+                lstProgramasAbiertos.Items.Add(procesosAbiertos[i].ProcessName);
+
+        }
+        public string[] ProgramasAñadidos
+        {
+            get
+            {
+                string[] programas = new string[lstProgramasAñadidos.Items.Count];
+                for (int i = 0; i < programas.Length; i++)
+                    programas[i] = lstProgramasAñadidos.Items[i].ToString();
+                return programas;
+            }
+        }
+
+        private void lstProgramasAbiertos_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (lstProgramasAbiertos.SelectedItem != null && !lstProgramasAñadidos.Items.Contains(lstProgramasAbiertos.SelectedItem))
+            {
+                lstProgramasAñadidos.Items.Add(lstProgramasAbiertos.SelectedItem);
+                lstProgramasAbiertos.Items.Remove(lstProgramasAbiertos.SelectedItem);
+            }
+        }
+
+        private void lstProgramasAñadidos_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (lstProgramasAñadidos.SelectedItem != null)
+                lstProgramasAñadidos.Items.Remove(lstProgramasAñadidos.SelectedItem);
+        }
+    }
+}
---
+++ ForzarCierre/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+namespace ForzarCierre
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Properties.Settings settings;
+        List<string> programasACerrar;
+        public MainWindow()
+        {
+            InitializeComponent();
+            settings = new Properties.Settings();
+            programasACerrar = new List<string>();
+            UpdateList();
+        }
+
+        private void UpdateList()
+        {
+            programasACerrar.Clear();
+            if (settings.ProgramasGuardados.Contains(';'))
+            {
+                programasACerrar.AddRange(settings.ProgramasGuardados.Split(';'));
+            }
+        }
+
+        private void btnCerrar_Click(object sender, RoutedEventArgs e)
+        {
+            Process[] porCerrar;
+            for (int i = 0; i < programasACerrar.Count; i++)
+            {
+                try
+                {
+                    porCerrar = System.Diagnostics.Process.GetProcessesByName(programasACerrar[i]);
+                    for (int j = 0; j < porCerrar.Length; j++)
+                        try
+                        {
+                            porCerrar[j].Kill();
+                        }
+                        catch { }
+                }
+                catch { }
+            }
+            this.Close();
+        }
+
+        private void btnCerrar_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            string[] programas;
+            AñadirProgramas winAñadirProgramas;
+            StringBuilder strProgramas;
+            if(Keyboard.IsKeyDown(Key.LeftCtrl))
+            {
+                winAñadirProgramas = new AñadirProgramas(programasACerrar);
+                winAñadirProgramas.ShowDialog();
+                programas = winAñadirProgramas.ProgramasAñadidos;
+                strProgramas = new StringBuilder("programa");
+                for (int i = 0; i < programas.Length; i++)
+                    strProgramas.Append(";" + programas[i]);
+                settings.ProgramasGuardados = strProgramas.ToString();
+                UpdateList();
+                settings.Save();
+            }
+        }
+    }
+}
